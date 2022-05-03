7aecfb133d5d15b4e552f1ab814e559c3d9bc08e
sachetwasti1997 <sachetwasti61@gmail.com>
2022-05-03 09:38:55 +0530

    Initialised the app

---
+++ src/main/kotlin/com/example/notes_user/NotesUserApplication.kt
@@ -0,0 +1,11 @@
+package com.example.notes_user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesUserApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotesUserApplication>(*args)
+}
