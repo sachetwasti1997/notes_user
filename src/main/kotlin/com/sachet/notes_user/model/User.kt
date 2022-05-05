11ba3b11d5a2f3042bafdb19c3f81ab024b96a25
sachetwasti1997 <sachetwasti61@gmail.com>
2022-05-05 12:14:59 +0530

    Added validations for fields in kotlin

--- src/main/kotlin/com/sachet/notes_user/model/User.kt
+++ src/main/kotlin/com/sachet/notes_user/model/User.kt
@@ -10,21 +10,21 @@ data class User(
     @Id
     @JsonView(Views.Base::class)
     var userId: String? = null,
-    @NotNull(value = "First Name cannot be null")
+    @field:NotNull(value = "First Name cannot be null")
     @JsonView(Views.Base::class)
     var firstName: String? = null,
     @JsonView(Views.Base::class)
     var middleName: String? = null,
-    @NotNull(value = "Last Name cannot be null")
+    @field:NotNull(value = "Last Name cannot be null")
     @JsonView(Views.Base::class)
     var lastName: String? = null,
-    @NotNull(value = "Last Name cannot be null")
+    @field:NotNull(value = "Last Name cannot be null")
     @JsonView(Views.Base::class)
     var email: String? = null,
-    @NotNull(value = "User Name cannot be null")
+    @field:NotNull(value = "User Name cannot be null")
     @JsonView(Views.Base::class)
     var userName: String? = null,
-    @NotNull(value = "Password cannot be null")
+    @field:NotNull(value = "Password cannot be null")
     var password: String? = null,
     var roles: List<String> ?= null
 )
