687fc45396e0655a143af76f1d27b931e1b93ae8
sachetwasti1997 <sachetwasti61@gmail.com>
2022-05-05 12:16:45 +0530

    Changed Imports

--- src/main/kotlin/com/sachet/notes_user/model/User.kt
+++ src/main/kotlin/com/sachet/notes_user/model/User.kt
@@ -1,30 +1,31 @@
 package com.sachet.notes_user.model
 
 import com.fasterxml.jackson.annotation.JsonView
-import org.jetbrains.annotations.NotNull
+
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import javax.validation.constraints.NotNull
 
 @Document
 data class User(
     @Id
     @JsonView(Views.Base::class)
     var userId: String? = null,
-    @field:NotNull(value = "First Name cannot be null")
+    @field:NotNull(message = "First Name cannot be null")
     @JsonView(Views.Base::class)
     var firstName: String? = null,
     @JsonView(Views.Base::class)
     var middleName: String? = null,
-    @field:NotNull(value = "Last Name cannot be null")
+    @field:NotNull(message = "Last Name cannot be null")
     @JsonView(Views.Base::class)
     var lastName: String? = null,
-    @field:NotNull(value = "Last Name cannot be null")
+    @field:NotNull(message = "Last Name cannot be null")
     @JsonView(Views.Base::class)
     var email: String? = null,
-    @field:NotNull(value = "User Name cannot be null")
+    @field:NotNull(message = "User Name cannot be null")
     @JsonView(Views.Base::class)
     var userName: String? = null,
-    @field:NotNull(value = "Password cannot be null")
+    @field:NotNull(message = "Password cannot be null")
     var password: String? = null,
     var roles: List<String> ?= null
 )
